1273b433af9b5df0a7901f14e1c14dcce35d3857
yoolalw <larakemilyprofissional@gmail.com>
2026-08-11 14:31:38 -0300

    tests: Verifying displayed items + Creating new applications

--- tests/pages/page_home.py
+++ tests/pages/page_home.py
@@ -1,8 +1,11 @@
+from time import sleep
+
 import pytest
 import allure
 from appium.webdriver.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 
 
@@ -11,7 +14,7 @@ class HomePage:
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
 
-        self.title = (By.XPATH, '//*[@id="root"]/div/header/div[1]/div/h1' )
+        self.title = (By.XPATH, '//*[@id="root"]/div/header/div[1]/div/h1')
 
         self.language_button = (By.XPATH, '//*[@id="root"]/div/header/div[2]/div/button')
         self.alert_button = (By.XPATH, '//*[@id="root"]/div/header/div[2]/button[1]')
@@ -20,35 +23,66 @@ class HomePage:
         # These XPATH are for the 'Ping Test Internet' instance. The execution begins with the JSON file.
         self.title_container = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[1]/div[1]/h3')
         self.status_container = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[1]/div[1]/div/span[2]')
-        self.status_container_color = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[1]/div[1]/div/span[1]')
+        self.status_container_color = (By.XPATH,
+                                       '//*[@id="root"]/div/main/section/div/div[2]/article[1]/div[1]/div/span[1]')
         self.start_button = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[1]/div[2]/div[2]/button[1]')
-        self.config_button = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[1]/div[2]/div[2]/button[2]')
+        self.config_button = (By.XPATH,
+                              '//*[@id="root"]/div/main/section/div/div[2]/article[1]/div[2]/div[2]/button[2]')
+        self.container_languages = (By.XPATH, '//*[@id="root"]/div/header/div[2]/div/div/button')
+        self.pt_language = (By.XPATH, '//*[@id="root"]/div/header/div[2]/div/div/button[1]')
+
+        self.container_title_new_app = (By.XPATH, '//*[@id="root"]/div/main/section/div[2]/aside/div/h2')
+        self.container_input_name_new_app = (By.XPATH, '/html/body/div/div/main/section/div[2]/aside/form/div[1]/section[1]/div/label[1]/input')
+        self.container_input_port_new_app = (By.XPATH, '//*[@id="root"]/div/main/section/div[2]/aside/form/div[1]/section[1]/div/div/label[1]/input')
+        self.container_input_directory_new_app = (By.XPATH, '//*[@id="root"]/div/main/section/div[2]/aside/form/div[1]/section[1]/div/div/label[2]/input')
+        self.container_input_command_new_app = (By.XPATH, '//*[@id="root"]/div/main/section/div[2]/aside/form/div[1]/section[1]/div/label[3]/input')
+        self.container_input_arguments_new_app = (By.XPATH, '//*[@id="root"]/div/main/section/div[2]/aside/form/div[1]/section[1]/div/label[4]/input')
+        self.container_input_web_link_new_app = (By.XPATH, '//*[@id="root"]/div/main/section/div[2]/aside/form/div[1]/section[1]/div/label[5]/input')
+        self.container_button_add_app_new_app = (By.XPATH, '//*[@id="root"]/div/main/section/div[2]/aside/form/div[2]/button[2]')
+        self.container_button_cancel_new_app = (By.XPATH, '//*[@id="root"]/div/main/section/div[2]/aside/form/div[2]/button[1]')
 
     @allure.step('Verifying fixed elements on the screen')
     def verifying_if_the_elements_are_displayed(self):
-        return self.driver.find_element(self.title).is_displayed() and \
-            self.driver.find_element(self.language_button) and \
-            self.driver.find_element(self.alert_button) and \
-            self.driver.find_element(self.new_application)
+        return self.driver.find_element(*self.title).is_displayed() and \
+            self.driver.find_element(*self.language_button).is_displayed() and \
+            self.driver.find_element(*self.alert_button).is_displayed() and \
+            self.driver.find_element(*self.new_application).is_displayed()
+
+    @allure.step('Selecting languages')
+    def selecting_language(self):
+        self.driver.find_element(*self.language_button).click()
+        sleep(1)
+        btns = self.driver.find_elements(*self.container_languages)
+        sleep(1)
+        for i in range(len(btns)):
+            btns = self.driver.find_elements(*self.container_languages)
+            btns[i].click()
+            self.driver.find_element(*self.language_button).click()
+            sleep(2)
 
     @allure.step('Verifying creation of the item "Ping Test Internet" ')
-    def container_created_from_json(self):
+    def container_auto_created_from_json(self):
         return self.wait.until(ec.visibility_of_element_located(self.title_container)) and \
             self.wait.until(ec.visibility_of_element_located(self.status_container)) and \
             self.wait.until(ec.visibility_of_element_located(self.status_container_color)) and \
             self.wait.until(ec.visibility_of_element_located(self.config_button)) and \
             self.wait.until(ec.visibility_of_element_located(self.start_button))
 
-    @allure.step('Clicking in language button')
-    def clicking_in_language_button(self):
-        self.driver.find_element(self.language_button).click()
-
     @allure.step('Clicking in alert button')
     def clicking_in_alert_button(self):
-        self.driver.find_element(self.alert_button).click()
+        self.driver.find_element(*self.alert_button).click()
 
     @allure.step('Clicking in new app button')
     def clicking_in_new_application(self):
-        self.driver.find_element(self.new_application).click()
+        self.driver.find_element(*self.new_application).click()
+        return self.wait.until(ec.visibility_of_element_located(self.container_title_new_app)).is_displayed()
+
+    def creating_application_with_web_link(self, input_name, input_port, input_web_link):
+        self.wait.until(ec.visibility_of_element_located(self.container_input_name_new_app)).send_keys(input_name)
+        self.wait.until(ec.visibility_of_element_located(self.container_input_port_new_app)).send_keys(input_port)
+        self.wait.until(ec.visibility_of_element_located(self.container_input_web_link_new_app)).send_keys(input_web_link)
+
+    def clicking_in_add_app(self):
+        self.wait.until(ec.visibility_of_element_located(self.container_button_add_app_new_app)).click()
 
 
--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,10 +1,14 @@
+from time import sleep
+
+import allure
 import pytest
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 from pages.page_home import HomePage
 
 from conftest import driver
-
+@allure.title('Tests in Page Home')
+@allure.description('The tests will consist the assertions from new created applications')
 @pytest.mark.usefixtures('driver')
 class TestHomePage:
     driver: WebDriver
@@ -14,8 +18,32 @@ class TestHomePage:
         self.driver.get('http://127.0.0.1:3000/')
         self.home_page = HomePage(self.driver)
 
-    def test_elements_displayed_on_the_screen(self):
-        assert self.home_page.verifying_if_the_elements_are_displayed
+    #PDT01
+    def test_verifying_if_home_are_running(self):
+        assert self.home_page.verifying_if_the_elements_are_displayed()
 
-    def test_creation_from_the_json_file(self):
-        assert self.home_page.container_created_from_json()
+    #PDT02
+    def test_verifying_if_the_auto_container_has_been_created(self):
+        assert self.home_page.container_auto_created_from_json()
+
+    #PDT03
+    def test_changes_the_language_and_checking_if_all_application_has_been_modified(self):
+        self.home_page.selecting_language()
+        assert True
+
+    #PDT04
+    def test_clicking_in_new_app_button(self):
+        assert self.home_page.clicking_in_new_application()
+
+    #PDT05
+    @pytest.mark.parametrize(
+        "input_name, input_port, input_web_link",
+        [
+            ('PokeApiWebLinkTest', '8231', 'https://pokeapi.co/')
+        ]
+    )
+    def test_starting_new_application(self):
+        self.home_page.clicking_in_new_application()
+        self.home_page.creating_application_with_web_link()
+
+

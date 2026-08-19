e9758e2bf4b818ef3b817fb19986ede4f35fbe22
yoolalw <larakemilyprofissional@gmail.com>
2026-08-19 14:34:33 -0300

    tests: Refazendo todos os testes na tela home de forma organizada.

--- tests/pages/page_home.py
+++ tests/pages/page_home.py
@@ -26,6 +26,8 @@ class PageHome:
         self.alert_button_mark_all_read = (By.XPATH, '//*[@id="root"]/div/main/section/div/div/div[2]/button[1]')
         self.alert_button_clear_all = (By.XPATH, '//*[@id="root"]/div/main/section/div/div/div[2]/button[2]')
         self.alert_container = (By.XPATH, '//*[@id="root"]/div/main/section/div/div/section/div')
+        self.alert_message_container = (By.XPATH, '//*[@id="root"]/div/main/section/div/div/section/ul/li')
+
         #new app
         self.new_app_button = (By.XPATH, '//*[@id="root"]/div/header/div[2]/button[2]')
         self.new_app_title = (By.XPATH, '//*[@id="root"]/div/main/section/div[2]/aside/div/h2')
@@ -36,12 +38,27 @@ class PageHome:
         self.new_app_input_arguments = (By.XPATH, '//*[@id="root"]/div/main/section/div[2]/aside/form/div[1]/section[1]/div/label[4]/input')
         self.new_app_input_button_add_app = (By.XPATH, '//*[@id="root"]/div/main/section/div[2]/aside/form/div[2]/button[2]')
         self.new_app_input_button_cancel = (By.XPATH, '//*[@id="root"]/div/main/section/div[2]/aside/form/div[2]/button[1]')
+
         #container ping_teste_internet
         self.container_ping_teste_internet = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article')
         self.container_ping_teste_internet_title = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article/div[1]/h3')
         self.container_ping_teste_internet_status = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article/div[1]/div/span[2]')
         self.container_ping_teste_internet_button_start = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article/div[2]/div[2]/button[1]')
         self.container_ping_teste_internet_button_config = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article/div[2]/div[2]/button[2]')
+
+        #container created
+        self.container_created = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[2]')
+        self.container_created_title = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[2]/div[1]/h3')
+        self.container_created_status = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[2]/div[1]/div/span[2]')
+        self.container_created_button_start = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[2]/div[2]/div[2]/button[1]')
+        self.container_created_button_config = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[2]/div[2]/div[2]/button[2]')
+
+        # container for negative tests
+        self.container_created_for_generate_alert = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[3]')
+        self.container_created_for_generate_alert_title = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[3]/div[1]/h3')
+        self.container_created_for_generate_alert_status = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[3]/div[1]/div/span[2]')
+        self.container_created_for_generate_alert_button_start = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[3]/div[2]/div[2]/button[1]')
+        self.container_created_for_generate_alert_button_config = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[3]/div[2]/div[2]/button[2]')
 
     #displayed elements
 
@@ -54,7 +71,35 @@ class PageHome:
     def verifying_if_ping_teste_internet_application_has_been_displayed(self):
         return self.wait.until(ec.text_to_be_present_in_element(self.container_ping_teste_internet_title, 'Ping Teste Internet'))
 
+    def verifying_if_the_container_has_been_created(self):
+        return self.wait.until(ec.visibility_of_element_located(self.container_created)) and \
+            self.wait.until(ec.visibility_of_element_located(self.container_created_title)) and \
+            self.wait.until(ec.visibility_of_element_located(self.container_created_status)) and \
+            self.wait.until(ec.visibility_of_element_located(self.container_created_button_config)) and \
+            self.wait.until(ec.visibility_of_element_located(self.container_created_button_start) )
+
+    def verifying_if_the_alert_has_been_generated(self):
+        return self.wait.until(ec.visibility_of_element_located(self.alert_message_container)).is_displayed()
+
     #click elements
+    def click_configurations_from_the_container_created(self):
+        self.wait.until(ec.visibility_of_element_located(self.container_created_button_config)).click()
+
+    def click_in_languages_button(self):
+        self.wait.until(ec.visibility_of_element_located(self.languages_button)).click()
+
+    def click_to_mark_all_alerts_read(self):
+        self.wait.until(ec.visibility_of_element_located(self.alert_button_mark_all_read)).click()
+
+    def click_to_clear_all_container_from_alerts(self):
+        self.wait.until(ec.visibility_of_element_located(self.alert_button_clear_all)).click()
+
+    def click_on_start_the_application_created(self):
+        self.wait.until(ec.visibility_of_element_located(self.container_created_button_start)).click()
+
+    def click_on_start_button_for_application_with_wrong_application(self):
+        self.wait.until(ec.visibility_of_element_located(self.container_created_for_generate_alert_button_start)).click()
+
     def click_on_new_app_button(self):
         self.wait.until(ec.visibility_of_element_located(self.new_app_button)).click()
         return self.wait.until(ec.visibility_of_element_located(self.new_app_title)).is_displayed() and \
@@ -69,12 +114,43 @@ class PageHome:
     def click_in_new_app_add_app_button(self):
         self.wait.until(ec.visibility_of_element_located(self.new_app_input_button_add_app)).click()
 
+    def click_in_alert_icon(self):
+        self.wait.until(ec.visibility_of_element_located(self.alert_button)).click()
+
     def click_in_new_app_cancel_button(self):
         self.wait.until(ec.visibility_of_element_located(self.new_app_input_button_cancel)).click()
 
+    #texts
+    def verifying_status_from_created_container(self):
+        return self.wait.until(ec.visibility_of_element_located(self.container_created_status)).text
 
+    def verifying_status_from_container_with_error_configuration(self):
+        return self.wait.until(ec.text_to_be_present_in_element(self.container_created_for_generate_alert_status, 'failed'))
 
     #inserting / alter elements
+
+    def changes_configuration_from_the_container_created(self, input_name, input_port, input_directory, input_command, input_arguments):
+        input_new_name = self.wait.until(ec.visibility_of_element_located(self.new_app_input_name))
+        input_new_name.clear()
+        input_new_name.send_keys(input_name)
+
+        input_new_port = self.wait.until(ec.visibility_of_element_located(self.new_app_input_port))
+        input_new_port.clear()
+        input_new_port.send_keys(input_port)
+
+        input_new_directory = self.wait.until(ec.visibility_of_element_located(self.new_app_input_directory))
+        input_new_directory.clear()
+        input_new_directory.send_keys(input_directory)
+
+        input_new_command = self.wait.until(ec.visibility_of_element_located(self.new_app_input_command))
+        input_new_command.clear()
+        input_new_command.send_keys(input_command)
+
+        input_new_arguments = self.wait.until(ec.visibility_of_element_located(self.new_app_input_arguments))
+        input_new_arguments.clear()
+        input_new_arguments.send_keys()
+
+
     def creating_new_application(self, input_name, input_port, input_directory, input_command, input_arguments):
         self.wait.until(ec.visibility_of_element_located(self.new_app_input_name)).send_keys(input_name)
         self.wait.until(ec.visibility_of_element_located(self.new_app_input_port)).send_keys(input_port)
@@ -84,9 +160,9 @@ class PageHome:
 
 
     def switch_system_language(self):
-        self.wait.until(ec.element_to_be_clickable(self.languages_button)).click()
-        all_languages = self.wait.until(ec.visibility_of_element_located(self.language))
-        for i in range(len(all_languages)):
-            specify_language = self.wait.until(ec.visibility_of_element_located(self.language))
-            actual_language = specify_language[i]
-            actual_language.click()
+        self.wait.until(ec.visibility_of_element_located(self.languages_button))
+        languages = self.driver.find_element(*self.languages_container)
+        for i in range(len(languages)):
+            languages = self.driver.find_element(*self.languages_container)
+            languages[i].click()
+            self.wait.until(ec.visibility_of_element_located(self.languages_button)).click()
---
+++ tests/tests_interface/test_page_home.py
@@ -0,0 +1,63 @@
+import pytest
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from tests.pages.page_home import PageHome
+from tests.conftest import driver
+
+@pytest.mark.usefixtures("driver")
+class TestPageHome:
+    driver: WebDriver
+    wait: WebDriverWait
+
+    def setup_method(self):
+        self.driver.get('http://127.0.0.1:3000/')
+        self.page_home = PageHome(self.driver)
+
+    #1
+    def test_verifying_if_the_elements_are_displayed(self):
+        assert self.page_home.verifying_elements_displayed_in_home()
+    #2
+    def test_verifying_existence_of_application_ping_test_internet(self):
+        assert self.page_home.verifying_if_ping_teste_internet_application_has_been_displayed()
+    #3
+    def test_switching_the_language_of_website(self):
+        self.page_home.click_in_languages_button()
+        self.page_home.switch_system_language()
+        assert True
+    #4
+    def test_click_and_open_container_new_application(self):
+        self.page_home.click_on_new_app_button()
+        #5
+        self.page_home.creating_new_application('App_Local_Test', '8080', 'C:\Blooms\BloomsKimono\jvApi', 'mvn', 'spring-boot:run')
+        self.page_home.click_in_new_app_add_app_button()
+        assert self.page_home.verifying_if_the_container_has_been_created()
+
+    #6
+    def test_starting_the_application_created(self):
+        self.page_home.click_on_start_the_application_created()
+        assert 'running' in self.page_home.verifying_status_from_created_container()
+
+    #7
+    def test_change_configuration_from_the_container_created(self):
+        self.page_home.click_configurations_from_the_container_created()
+        self.page_home.changes_configuration_from_the_container_created('New Name', '8080', 'C:\ryuSa\RyuFront\RyuStockSA', 'npx', 'live-server --port=2210')
+        self.page_home.click_on_new_app_button()
+        assert True
+
+    #8
+    def test_starting_the_application_with_new_configuration(self):
+        self.page_home.click_on_start_the_application_created()
+        assert 'running' in self.page_home.verifying_status_from_created_container()
+
+    #9
+    def test_verifying_if_the_alert_has_been_generated(self):
+        self.page_home.creating_new_application('Application With Error', '2222', 'wrong', 'wrong', 'wrong')
+        self.page_home.click_on_start_button_for_application_with_wrong_application()
+        assert self.page_home.verifying_status_from_container_with_error_configuration()
+        self.page_home.click_in_alert_icon()
+        assert self.page_home.verifying_if_the_alert_has_been_generated()
+        #10
+        self.page_home.click_to_mark_all_alerts_read()
+        #11
+        self.page_home.click_to_clear_all_container_from_alerts()
+        assert False in self.page_home.verifying_if_the_alert_has_been_generated()

1491b1dd00052d459611d3531074b05380707c7d
yoolalw <larakemilyprofissional@gmail.com>
2026-08-31 14:42:10 -0300

    tests: E2E - Home / Finalizado

--- tests/pages/page_home.py
+++ tests/pages/page_home.py
@@ -3,6 +3,7 @@ from time import sleep
 import pytest
 import allure
 from dotenv import set_key
+from selenium.common import StaleElementReferenceException
 from selenium.webdriver.support import expected_conditions as ec
 
 
@@ -55,6 +56,7 @@ class PageHome:
         self.container_created_title = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[2]/div[1]/h3')
         self.container_created_status = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[2]/div[1]/div/span[2]')
         self.container_created_button_start = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[2]/div[2]/div[2]/button[2]')
+
         self.container_created_button_config = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[2]/div[2]/div[2]/button[3]')
 
         # container for negative tests
@@ -65,17 +67,18 @@ class PageHome:
         self.container_created_for_generate_alert_button_config = (By.XPATH, '//*[@id="root"]/div/main/section/div/div[2]/article[3]/div[2]/div[2]/button[3]')
 
     #displayed elements
-
-
+    @allure.step("Verifying if the elements are displayed in Home Page")
     def verifying_elements_displayed_in_home(self):
         return self.wait.until(ec.visibility_of_element_located(self.page_title)).is_displayed() and \
             self.wait.until(ec.visibility_of_element_located(self.languages_button)).is_displayed() and \
             self.wait.until(ec.visibility_of_element_located(self.alert_button)).is_displayed() and \
             self.wait.until(ec.visibility_of_element_located(self.new_app_button)).is_displayed()
 
+    @allure.step("Verifying if the 'Ping Teste Internet' has been created")
     def verifying_if_ping_teste_internet_application_has_been_displayed(self):
         return self.wait.until(ec.text_to_be_present_in_element(self.container_ping_teste_internet_title, 'Ping Teste Internet'))
 
+    @allure.step("Checking all elements in container displayed - Ping Teste Internet")
     def verifying_if_the_container_has_been_created(self):
         return self.wait.until(ec.visibility_of_element_located(self.container_created)).is_displayed() and \
             self.wait.until(ec.visibility_of_element_located(self.container_created_title)).is_displayed() and \
@@ -83,31 +86,40 @@ class PageHome:
             self.wait.until(ec.visibility_of_element_located(self.container_created_button_config)).is_displayed() and \
             self.wait.until(ec.visibility_of_element_located(self.container_created_button_start)).is_displayed()
 
+    @allure.step("Verifying alert message displayed")
     def verifying_if_the_alert_has_been_generated(self):
         return self.wait.until(ec.visibility_of_element_located(self.alert_message_container)).is_displayed()
 
     #click elements
+    @allure.step("Click in language -> Portuguese")
     def click_to_change_language_for_portuguese(self):
         self.wait.until(ec.visibility_of_element_located(self.language)).click()
 
+    @allure.step("Click in configurations from the created container")
     def click_configurations_from_the_container_created(self):
         self.wait.until(ec.visibility_of_element_located(self.container_created_button_config)).click()
 
+    @allure.step("Click in button to change website language ")
     def click_in_languages_button(self):
         self.wait.until(ec.visibility_of_element_located(self.languages_button)).click()
 
+    @allure.step("Click in button to mark all alerts read")
     def click_to_mark_all_alerts_read(self):
         self.wait.until(ec.visibility_of_element_located(self.alert_button_mark_all_read)).click()
 
+    @allure.step("Click in button to clear all alert container")
     def click_to_clear_all_container_from_alerts(self):
         self.wait.until(ec.visibility_of_element_located(self.alert_button_clear_all)).click()
 
+    @allure.step("Click in button to start the created application")
     def click_on_start_the_application_created(self):
-        self.wait.until(ec.visibility_of_element_located(self.container_created_button_start)).click()
+        self.wait.until(ec.element_to_be_clickable(self.container_created_button_start)).click()
 
+    @allure.step("Click in button to start the application with errors")
     def click_on_start_button_for_application_with_wrong_application(self):
         self.wait.until(ec.visibility_of_element_located(self.container_created_for_generate_alert_button_start)).click()
 
+    @allure.step("Click in 'New App+' button")
     def click_on_new_app_button(self):
         self.wait.until(ec.visibility_of_element_located(self.new_app_button)).click()
         return self.wait.until(ec.visibility_of_element_located(self.new_app_title)).is_displayed() and \
@@ -118,28 +130,46 @@ class PageHome:
             self.wait.until(ec.visibility_of_element_located(self.new_app_input_arguments)) .is_displayed() and \
             self.wait.until(ec.visibility_of_element_located(self.new_app_input_button_add_app)).is_displayed() and \
             self.wait.until(ec.visibility_of_element_located(self.new_app_input_button_cancel)).is_displayed()
-
+    @allure.step("Click in 'Add App' in 'New App+' container")
     def click_in_new_app_add_app_button(self):
         self.wait.until(ec.visibility_of_element_located(self.new_app_input_button_add_app)).click()
 
+    @allure.step("Click in 'Alert' button")
     def click_in_alert_icon(self):
         self.wait.until(ec.visibility_of_element_located(self.alert_button)).click()
 
+    @allure.step("Click in 'Cancel' in 'New App+' container")
     def click_in_new_app_cancel_button(self):
         self.wait.until(ec.visibility_of_element_located(self.new_app_input_button_cancel)).click()
 
+    @allure.step("Click in start button after the changes")
+    def click_in_start_application_after_changes(self):
+        for attempt in range(3):
+            try:
+                self.wait.until(
+                    ec.element_to_be_clickable(
+                        self.container_created_button_start
+                    )
+                ).click()
+                return
+            except StaleElementReferenceException:
+                if attempt == 2:
+                    raise
     #texts
+    @allure.step("Verifying the text returned from title in 'Alert' container")
     def verifying_text_from_alerts_container(self):
         return self.wait.until(ec.visibility_of_element_located(self.alert_container)).text
 
+    @allure.step("Verifying the status from the created container")
     def verifying_status_from_created_container(self):
         return self.wait.until(ec.visibility_of_element_located(self.container_created_status)).text
 
+    @allure.step("Verifying status with error configuration container")
     def verifying_status_from_container_with_error_configuration(self):
         return self.wait.until(ec.text_to_be_present_in_element(self.container_created_for_generate_alert_status, 'failed'))
 
     #inserting / alter elements
-
+    @allure.step("Change the configuration from the new container created")
     def changes_configuration_from_the_container_created(self, input_name, input_port, input_directory, input_command, input_arguments):
         input_new_name = self.wait.until(ec.visibility_of_element_located(self.new_app_input_name))
         input_new_name.clear()
@@ -159,9 +189,9 @@ class PageHome:
 
         input_new_arguments = self.wait.until(ec.visibility_of_element_located(self.new_app_input_arguments))
         input_new_arguments.clear()
-        input_new_arguments.send_keys()
+        input_new_arguments.send_keys(input_arguments)
 
-
+    @allure.step('Creating new application')
     def creating_new_application(self, input_name, input_port, input_directory, input_command, input_arguments):
         self.wait.until(ec.visibility_of_element_located(self.new_app_input_name)).send_keys(input_name)
         self.wait.until(ec.visibility_of_element_located(self.new_app_input_port)).send_keys(input_port)
@@ -169,6 +199,7 @@ class PageHome:
         self.wait.until(ec.visibility_of_element_located(self.new_app_input_command)).send_keys(input_command)
         self.wait.until(ec.visibility_of_element_located(self.new_app_input_arguments)).send_keys(input_arguments)
 
+    @allure.step("Switch the system language")
     def switch_system_language(self):
         self.driver.find_element(*self.languages_button).click()
         container = self.driver.find_elements(*self.languages_container)
--- tests/tests_e2e/test_page_home_e2e.py
+++ tests/tests_e2e/test_page_home_e2e.py
@@ -1,6 +1,9 @@
+from time import sleep
+
 import allure
 import pytest
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 from tests.conftest import driver
@@ -18,20 +21,29 @@ class TestHomeE2E:
         self.driver.get('http://127.0.0.1:3000/')
         self.page_home = PageHome(self.driver)
 
-    @allure.title('Flux of tests E2E - ID: TU01')
+    @allure.title('Flux of tests E2E - HID: HTU01')
     def test_user_creating_new_applications_in_website(self):
         self.page_home.click_in_languages_button()
         self.page_home.click_to_change_language_for_portuguese()
         self.page_home.click_on_new_app_button()
-        self.page_home.creating_new_application('Starting java application', '8080', 'C:\Blooms\BloomsKimono\jvApi', 'mvn', 'spring-boot:run')
+        self.page_home.creating_new_application('Starting java application', '80', 'C:\Blooms\BloomsKimono\jvApi',
+                                                'mvn', 'spring-boot:run')
         self.page_home.click_in_new_app_add_app_button()
+        self.page_home.click_on_start_the_application_created()
+        self.page_home.click_on_start_the_application_created()
+        self.wait.until(
+            expected_conditions.text_to_be_present_in_element(self.page_home.container_created_status, 'running'))
+
         self.page_home.click_on_start_the_application_created()
         self.page_home.click_configurations_from_the_container_created()
-        self.page_home.changes_configuration_from_the_container_created('New application', '8080', 'C:\Blooms\BloomsKimono\jvApi', 'spring/boot-run', 'mvl')
+        self.page_home.changes_configuration_from_the_container_created('New application', '8080',
+                                                                        'C:\Blooms\BloomsKimono\jvApi',
+                                                                        'spring/boot-run', 'mvl')
         self.page_home.click_in_new_app_add_app_button()
-        self.page_home.click_on_start_the_application_created()
+        self.page_home.click_in_start_application_after_changes()
+        self.wait.until(expected_conditions.text_to_be_present_in_element(self.page_home.container_created_status, 'failed'))
         self.page_home.click_in_alert_icon()
         self.page_home.verifying_if_the_alert_has_been_generated()
         self.page_home.click_to_mark_all_alerts_read()
         self.page_home.click_to_clear_all_container_from_alerts()
-        assert self.page_home.verifying_text_from_alerts_container()
+        assert 'Nenhum alerta registrado.' in self.page_home.verifying_text_from_alerts_container()
